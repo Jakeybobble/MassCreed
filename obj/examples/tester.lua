3752415f35d866cd83fbbe2e78d4daefcbb2ba0f
Jacob G.S. <jacobgsdev@gmail.com>
2024-11-02 15:27:08 +0100

    Moved files and added comments

---
+++ obj/examples/tester.lua
@@ -0,0 +1,17 @@
+-- The class will inherit from the Dad class
+inherit("Dad")
+
+-- The class will be initialized with these variables
+heck = 1
+
+-- The created 30log class is created and then passed over for use in this file :-)
+function class:init()
+    self.super.init()
+    print(self.name.." has been instantiated!")
+end
+
+function class:update()
+    print(self.heck)
+    self.heck = self.heck + 1
+    print(parent.name)
+end
