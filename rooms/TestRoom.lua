27276919390b3cc3b765eecc11b4e43016a37669
Jacob G.S. <jacobgsdev@gmail.com>
2024-11-08 21:23:07 +0100

    Basic tilemap

--- rooms/TestRoom.lua
+++ rooms/TestRoom.lua
@@ -7,6 +7,26 @@ function class:init()
 
      local new_obj = classes.TestWorm:new(100, 100)
      table.insert(self.objects, new_obj)
+     
+
+    local image = love.graphics.newImage("assets/tilemap/debug.png")
+    local w, h = image:getDimensions()
+    self.tiles_batch = {}
+
+    self.batch = love.graphics.newSpriteBatch(image)
+
+    for y = 0, h - 16, 16 do
+        for x = 0, w - 16, 16 do
+            local quad = love.graphics.newQuad(x, y, 16, 16, w, h)
+            table.insert(self.tiles_batch, quad)
+        end
+    end
+
+    self.tiles = {
+        {1, 5, 10}, {2, 6, 10}, {2, 7, 10}, {2, 8, 10}, {2, 9, 10}, {3, 10, 10}
+    }
+
+    love.graphics.setBackgroundColor(0.157, 0.208, 0.251)
 
 end
 
@@ -19,6 +39,18 @@ function class:update(dt)
 end
 
 function class:draw()
+
+    
+
+    -- Draw test tilemap
+    self.batch:clear()
+    for _, v in pairs(self.tiles) do
+        self.batch:add(self.tiles_batch[v[1]], v[2] * 16, v[3] * 16)
+    end
+    
+
+    love.graphics.draw(self.batch)
+
     -- Todo: Draw order by depth
     for k, v in pairs(self.objects) do
         v:draw()
