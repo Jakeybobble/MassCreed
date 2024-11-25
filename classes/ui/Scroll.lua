94e0382b24b9dd62520117a92ff7b827535338b4
Jacob G.S. <jacobgsdev@gmail.com>
2024-11-25 01:32:51 +0100

    Created Scroll element

---
+++ classes/ui/Scroll.lua
@@ -0,0 +1,11 @@
+-- Scroll.lua: Draws first child in canvas and adds scrolling if child dimensions are larger
+inherit("Element")
+function class:init(options, elements)
+    class.super.init(self, options, elements)
+
+    self.orientation = options.orientation or "vertical"
+    self.scroll_x, self.scroll_y = options.scroll_x or 0, options.scroll_y or 0
+
+    self.canvas = nil
+
+end
