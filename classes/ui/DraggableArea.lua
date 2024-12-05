77355c2f6a6e726e62f4fca0e20a986a9dc2c841
Jacob G.S. <jacobgsdev@gmail.com>
2024-12-05 22:07:42 +0100

    Prepared new DraggableArea element

---
+++ classes/ui/DraggableArea.lua
@@ -0,0 +1,12 @@
+-- DraggableArea: An area inside an element that moves (configurable) parent position when dragged.
+-- Bounds are the parent's parent.
+inherit("Element")
+local gui_handler = require("jakeylib/gui_handler")
+
+function class:init(options, elements)
+    class.super.init(self, options, elements)
+
+    -- Which parent within the same tree upwards to move
+    self.parent_depth = options.parent_depth or 1
+
+end
